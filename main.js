82d1688a36712ccd39c3854870003b5cc8facc54
Your Name <you@example.com>
2022-04-25 10:46:10 +0800

    create function in main.js

--- main.js
+++ main.js
@@ -8,3 +8,29 @@ const num = 13;
 export {
   obj
 }
+
+class Situation {
+  String where;
+  String doing;
+  String tool;
+
+  constructor(where, doing, tool) {
+    this.where = where;
+    this.doing = doing;
+    this.tool = tool;
+  }
+}
+
+class Person extends Situation {
+  int name;
+  int age;
+  constructor(name, age, where, doing, tool) {
+    this.super(where, doing, tool);
+    this.name = name;
+    this.age = age;
+  }
+  public void printDoing() {
+    输出句子
+  }
+}
+
